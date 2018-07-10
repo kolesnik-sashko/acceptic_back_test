40d815c3b32f39a0fe2b92cd61673fbff5cebaeb
kolesnik-sashko <kolesnik.aleksandr777@gmail.com>
2018-07-10 18:27:35 +0300

    add grid colection and shopper source model

---
+++ Acceptic/TestRest/Model/ResourceModel/Order/Grid/Collection.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Acceptic\TestRest\Model\ResourceModel\Order\Grid;
+
+use Psr\Log\LoggerInterface;
+use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Search\AggregationInterface;
+use Magento\Framework\Data\Collection\EntityFactoryInterface;
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Document;
+
+use Acceptic\TestRest\Model\ResourceModel\Order\Collection as GridCollection;
+use Acceptic\TestRest\Api\Schema\OrderSchemaInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+class Collection extends GridCollection implements SearchResultInterface
+{
+    public function __construct(
+        EntityFactoryInterface $entityFactory,
+        LoggerInterface $logger,
+        FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        AdapterInterface $adapter,
+        StoreManagerInterface $storeManager,
+        $mainTable,
+        $eventPrefix,
+        $eventObject,
+        $resourceModel,
+        $model = Document::class,
+        $connection = null,
+        AbstractDb $resource = null
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $adapter,
+            $storeManager,
+            $connection,
+            $resource
+        );
+        $this->_eventPrefix = $eventPrefix;
+        $this->_eventObject = $eventObject;
+        $this->_init($model, $resourceModel);
+        $this->setMainTable($mainTable);
+    }
+    /**
+     * @return AggregationInterface
+     */
+    public function getAggregations()
+    {
+        return $this->aggregations;
+    }
+    /**
+     * @param AggregationInterface $aggregations
+     *
+     * @return $this
+     */
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+    }
+    /**
+     * Get search criteria.
+     *
+     * @return \Magento\Framework\Api\SearchCriteriaInterface|null
+     */
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+    /**
+     * Set search criteria.
+     *
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     *
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setSearchCriteria(
+        \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null
+    ) {
+        return $this;
+    }
+    /**
+     * Get total count.
+     *
+     * @return int
+     */
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+    /**
+     * Set total count.
+     *
+     * @param int $totalCount
+     *
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+    /**
+     * Set items list.
+     *
+     * @param \Magento\Framework\Api\ExtensibleDataInterface[] $items
+     *
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+        $this->getSelect()->joinLeft(
+            ['shopperTable' => $this->getTable(OrderSchemaInterface::TABLE_NAME)],
+            'main_table.shopper_id = authorTable.shopper_id',
+            [
+                'shopper_name' => "CONCAT(shopperTable.name, ' ', shopperTable.last_name)"
+            ]
+        );
+    }
+}
---
+++ Acceptic/TestRest/Model/Source/ShoppersSelect.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 10.07.18
+ * Time: 17:39
+ */
+
+namespace Acceptic\TestRest\Model\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+use Acceptic\TestRest\Model\ResourceModel\Order\CollectionFactory;
+
+class ShoppersSelect implements ArrayInterface
+{
+    protected $shoppersCollection;
+
+    public function __construct(
+        CollectionFactory $factory
+    )
+    {
+        $this->shoppersCollection = $factory->create();
+    }
+    public function toOptionArray()
+    {
+        $result = [];
+        foreach ($this->shoppersCollection->getData() as $item)
+        {
+            $result[] = ['value' => $item['shopper_id'], 'label' => $item['name'] . ' ' . $item['last_name']];
+        }
+        return $result;
+    }
+}
