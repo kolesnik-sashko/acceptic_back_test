86e0a73882f4b63ba3b5362cdaef2b9c2744a31c
Sofia Khizhnia <sofabbk@gmail.com>
2018-07-11 08:03:45 +0300

    add order buttons

---
+++ Acceptic/TestRest/Block/Adminhtml/Order/Edit/GenericButton.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Acceptic\TestRest\Block\Adminhtml\Order\Edit;
+
+use Magento\Backend\Block\Widget\Context;
+
+use Acceptic\TestRest\Controller\Adminhtml\Order\Delete;
+
+class GenericButton
+{
+    /**
+     * GenericButton constructor.
+     * @param Context $context
+     */
+    public function __construct(
+        Context $context
+    ) {
+        $this->context = $context;    
+    }
+
+    /**
+     * @return string
+     */
+    public function getBackUrl()
+    {
+        return $this->getUrl('*/*/');
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeleteUrl()
+    {
+        return $this->getUrl('*/*/delete', [Delete::QUERY_PARAM_ID => $this->getObjectId()]);
+    }
+
+    /**
+     * @param string $route
+     * @param array $params
+     * @return string
+     */
+    public function getUrl($route = '', $params = [])
+    {
+        return $this->context->getUrlBuilder()->getUrl($route, $params);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getObjectId()
+    {
+        return $this->context->getRequest()->getParam(Delete::QUERY_PARAM_ID);
+    }     
+}
